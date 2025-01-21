5a056d0c6a3e6bafd496919518dc2c7e66c66d4f
Demis Rincon <darmfma@gmail.com>
2025-01-21 15:38:45 -0600

    footer fix

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -7,6 +7,11 @@ import Link from "next/link";
 import { useState } from "react";
 import styled from "styled-components";
 import WrapperFadeOnView from "./WrapperFadeOnView";
+import { navData } from "@/library/data/navigator";
+
+interface MainLayoutProps {
+  children: ReactNode;
+}
 
 export const Container = styled.div`
   max-width: 100vw;
@@ -80,12 +85,27 @@ const StyledText = styled(motion.span)`
   font-size: 1.2rem;
 `;
 
-const FooterContainer = styled.footer`
-  background-color: #333;
+const BottomContainer = styled.div`
   color: white;
+  background-color: #333;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const FooterContainer = styled.footer`
   padding: 1rem;
   text-align: center;
-  width: 100%;
+  width: 80%;
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 1rem;
+  justify-items: center;
+
+  @media (min-width: ${(props) => props.theme.breakpoints.lg}) {
+    grid-template-columns: repeat(3, 1fr);
+  }
 `;
 
 interface NavaLinkProps {
@@ -97,9 +117,36 @@ const NavaLink = ({ href, children }: NavaLinkProps) => (
   <StyledLink href={href}>{children}</StyledLink>
 );
 
-interface MainLayoutProps {
-  children: ReactNode;
-}
+const Span = styled.span`
+  color: ${(props) => props.theme.colors.white};
+  font-family: ${(props) => props.theme.fonts.navLink};
+  font-size: 1.2rem;
+  margin: 1rem 0.5rem;
+`;
+
+const SideContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  flex-direction: column;
+`;
+
+const ItemContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const RightContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  flex-direction: column;
+`;
+
+const Image = styled.img`
+  width: 100px;
+  height: auto;
+`;
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   const { scrollYProgress } = useScroll();
@@ -123,29 +170,34 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
         <SolidBackground style={{ opacity, y }} />
         {logoSrc && <Logo src={logoSrc} alt="logo" />}
         <Nav>
-          <NavaLink href="/">
-            <StyledText>Home</StyledText>
-          </NavaLink>
-          <NavaLink href="/about">
-            <StyledText>Nosotros</StyledText>
-          </NavaLink>
-          <NavaLink href="/gallery">
-            <StyledText>Galería</StyledText>
-          </NavaLink>
-          <NavaLink href="/location">
-            <StyledText>Ubicación</StyledText>
-          </NavaLink>
-          <NavaLink href="/contact">
-            <StyledText>Contacto</StyledText>
-          </NavaLink>
+          {navData.links.map((link) => (
+            <NavaLink key={link.href} href={link.href}>
+              <StyledText>{link.label}</StyledText>
+            </NavaLink>
+          ))}
         </Nav>
       </HeaderContainer>
       {children}
-      <WrapperFadeOnView>
-        <FooterContainer>
-          <p>© 2021 Hapkido. Todos los derechos reservados.</p>
-        </FooterContainer>
-      </WrapperFadeOnView>
+      <BottomContainer>
+        <WrapperFadeOnView>
+          <FooterContainer>
+            {navData.footerContent.content.map((content) => (
+              <SideContainer>
+                {content.data.map((item) => (
+                  <ItemContainer>
+                    {item.icon && <item.icon size={32} />}
+                    {item.image && <Image src={item.image} />}
+                    <Span key={item.text}>{item.text}</Span>
+                  </ItemContainer>
+                ))}
+              </SideContainer>
+            ))}
+
+            <WrapperFadeOnView></WrapperFadeOnView>
+          </FooterContainer>
+        </WrapperFadeOnView>
+        <p>{navData.footerContent.bottom}</p>
+      </BottomContainer>
     </Container>
   );
 };
---
+++ library/data/navigator.ts
@@ -0,0 +1,52 @@
+import { BiPhone } from "react-icons/bi";
+
+import { FiMail } from "react-icons/fi";
+import logo from "@/library/images/logo.png";
+import { GoLocation } from "react-icons/go";
+import { BsPerson } from "react-icons/bs";
+
+export const navData = {
+  links: [
+    { href: "/", label: "Inicio" },
+    { href: "/about", label: "Nosotros" },
+    { href: "/location", label: "Ubicación" },
+    { href: "/gallery", label: "Galería" },
+    { href: "/contact", label: "Contacto" },
+  ],
+  footerContent: {
+    content: [
+      {
+        data: [
+          {
+            text: "Pagina creada por Demis Rincon darmfma@gmail.com",
+            icon: BsPerson,
+          },
+        ],
+      },
+      {
+        data: [
+          {
+            image: logo.src,
+          },
+        ],
+      },
+      {
+        data: [
+          {
+            icon: GoLocation,
+            text: "Almolonia 99-2do piso,",
+          },
+          {
+            icon: BiPhone,
+            text: "312 111 1111",
+          },
+          {
+            icon: FiMail,
+            text: "hapkido@gmail.com",
+          },
+        ],
+      },
+    ],
+    bottom: "© 2021 Hapkido. Todos los derechos reservados.",
+  },
+};
