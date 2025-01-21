9079256c7b7d1df1af321a40febeed8933ec8906
Demis Rincon <darmfma@gmail.com>
2025-01-21 03:02:12 -0600

    remove unused code

--- app/about/hapkidoForAll.tsx
+++
@@ -1,7 +0,0 @@
-import { PageLayout } from "@/components/cells/styled";
-///images hap12 ninos - hap14 para adultos
-const HapkidoForAll = () => {
-  return <PageLayout>HAPKIDO PARA TODOS</PageLayout>;
-};
-
-export default HapkidoForAll;
