0641166fe794805f27d2539b3143ca4806c0b2ea
Demis Rincon <darmfma@gmail.com>
2025-02-17 01:46:56 -0600

    idding icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,16 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Hapkido Colima",
   description: "Hapkido Colima",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+    other: [
+      { url: "/android-chrome-192x192.png", sizes: "192x192" },
+      { url: "/android-chrome-512x512.png", sizes: "512x512" },
+      { url: "/favicon-16x16.png", sizes: "32x32" },
+      { url: "/favicon-16x16.png", sizes: "16x16" },
+    ],
+  },
 };
 
 export default function RootLayout({
