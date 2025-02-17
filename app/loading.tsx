a4b96bb44a1d476dbed0b000cb0d10a37db9c3c1
Demis Rincon <darmfma@gmail.com>
2025-02-17 01:28:16 -0600

    loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,40 +1,61 @@
 "use client";
 import React from "react";
+import { motion } from "framer-motion";
 import styled, { keyframes } from "styled-components";
 
-const spin = keyframes`
-    from {
-        transform: rotate(0deg);
-    }
-    to {
-        transform: rotate(360deg);
-    }
+const spinnerAnimation = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
 `;
 
-const Container = styled.div`
+export const SpinnerContainer = styled(motion.div)`
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
+  align-items: center;
   height: 100vh;
-  background-color: #f0f0f0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.white}; /* #FFFFFF */
+  z-index: 1000;
+  overflow: hidden;
 `;
 
-const Spinner = styled.div`
-  width: 50px;
-  height: 50px;
-  border: 5px solid #ccc;
-  border-top: 5px solid #1d72b8;
+export const Spinner = styled.div`
+  border: 16px solid ${({ theme }) => theme.colors.grey}; /* #e9e7e4 */
+  border-top: 16px solid ${({ theme }) => theme.colors.lightBlue}; /* #117DBF */
   border-radius: 50%;
-  animation: ${spin} 1s linear infinite;
+  width: 80px;
+  height: 80px;
+  animation: ${spinnerAnimation} 2s linear infinite;
+  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    width: 120px;
+    height: 120px;
+  }
 `;
 
-const Loading: React.FC = () => {
+export const LoadingText = styled.h1`
+  margin-top: 20px;
+  color: ${({ theme }) => theme.colors.darkBlue}; /* #2A3659 */
+  letter-spacing: 0.5rem;
+`;
+
+const Loading = () => {
+  const containerAnimation = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
+  };
+
   return (
-    <Container>
-      <Spinner />
-      <p>Loading...</p>
-    </Container>
+    <SpinnerContainer {...containerAnimation} data-testid="spinner">
+      <Spinner data-testid="spinner" />
+      <LoadingText>Loading...</LoadingText>
+    </SpinnerContainer>
   );
 };
 
