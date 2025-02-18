7ce41f5db8f74d6a9ce5043086b1500a196df627
Demis Rincon <darmfma@gmail.com>
2025-02-17 19:58:25 -0600

    fixes to cache

--- app/api/confluence/route.ts
+++ app/api/confluence/route.ts
@@ -35,7 +35,7 @@ export async function GET(request: Request) {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ query }),
-        cache: "no-store",
+        cache: "no-cache",
       }
     );
 
